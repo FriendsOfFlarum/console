1f6338a34411fb4b8a900f67e3da8a5a2c11396a
Daniël Klabbers <daniel@klabbers.email>
2017-10-17 06:08:06 +0200

    prevent method conflict and added dependency to allow scheduling

--- src/Providers/ConsoleProvider.php
+++ src/Providers/ConsoleProvider.php
@@ -6,6 +6,7 @@ use Flagrow\Console\Events\ConfigureConsoleApplication;
 use Flarum\Debug\Console\CacheClearCommand;
 use Flarum\Debug\Console\InfoCommand;
 use Flarum\Foundation\AbstractServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Console\Scheduling\ScheduleRunCommand;
 use Illuminate\Contracts\Events\Dispatcher as Events;
 
@@ -25,15 +26,17 @@ class ConsoleProvider extends AbstractServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(Schedule::class);
+
         $this->app
             ->make(Events::class)
-            ->listen(ConfigureConsoleApplication::class, [$this, 'commands']);
+            ->listen(ConfigureConsoleApplication::class, [$this, 'addCommands']);
     }
 
     /**
      * @param ConfigureConsoleApplication $event
      */
-    public function commands(ConfigureConsoleApplication $event)
+    public function addCommands(ConfigureConsoleApplication $event)
     {
         if (!$this->app->isInstalled()) {
             return;
